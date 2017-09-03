466167e36569ca11e099724344946ddce2af09f7
Michael Pope <mpope.cr@gmail.com>
2017-09-03 14:56:47 -0700

    Add &module?/1

--- lib/maximum_jank.ex
+++ lib/maximum_jank.ex
@@ -2,4 +2,14 @@ defmodule MaximumJank do
   @moduledoc """
   Documentation for MaximumJank.
   """
+
+  @doc """
+  Returns `true` if argument is a module, and `false` for anything else.
+  """
+  @spec module?(any()) :: boolean()
+  def module?(arg) do
+    arg.module_info()[:module] == Module.concat([arg])
+  rescue
+    _ -> false
+  end
 end
--- test/maximum_jank_test.exs
+++ test/maximum_jank_test.exs
@@ -1,3 +1,15 @@
 defmodule MaximumJankTest do
   use ExUnit.Case
+  import MaximumJank
+
+  describe "&module?/1" do
+    test "is true",  do: assert module?(MaximumJank) == true
+
+    test "is false" do
+      assert module?(FakeModule) == false
+      assert module?(123) == false
+      assert module?(%{}) == false
+      assert module?({1,2,3}) == false
+    end
+  end
 end
