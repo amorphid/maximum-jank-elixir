bb928a61de951430899d6cef741ed00de203d57d
Michael Pope <mpope.cr@gmail.com>
2017-09-03 14:36:01 -0700

    Delete mix kruft

--- lib/maximum_jank.ex
+++ lib/maximum_jank.ex
@@ -2,17 +2,4 @@ defmodule MaximumJank do
   @moduledoc """
   Documentation for MaximumJank.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MaximumJank.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/maximum_jank_test.exs
+++ test/maximum_jank_test.exs
@@ -1,8 +1,3 @@
 defmodule MaximumJankTest do
   use ExUnit.Case
-  doctest MaximumJank
-
-  test "greets the world" do
-    assert MaximumJank.hello() == :world
-  end
 end
