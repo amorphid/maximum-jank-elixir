1690d0f5b957962c12e5928fb431c38807648cf4
Michael Pope <mpope.cr@gmail.com>
2017-09-07 18:02:27 -0700

    Add &__using__/1, &__using__!/1, and &__using__?/1

--- lib/maximum_jank.ex
+++ lib/maximum_jank.ex
@@ -4,6 +4,86 @@ defmodule MaximumJank do
   """
 
   @doc """
+  TODO:  Come up w/ a non-lame way to document how awesome declaring `use MaximumJank` actually is; a good example is needed!
+  """
+  defmacro __using__(_) do
+    # throws an error if used outside of a module
+    if __CALLER__.module == nil do
+      raise "cannot invoke 'use #{inspect(__MODULE__)}' outside module"
+    end
+
+    # concatenates caller's module w/ current module
+    # example:  MyModule + __MODULE__ becomes MyModule.__MODULE__
+    module = Module.concat([__CALLER__.module, __MODULE__])
+
+    # checks to see if `use MaximumJank` has been called previously:
+    # - this check is done by calling `module.using?`
+    # - an `UndefinedFunctionError` means `use MaximumJank` has not been called
+    # - `true` means it has not been called, and the in the rescue block is run
+    try do
+      module.using?()
+    rescue
+      _ in UndefinedFunctionError ->
+        Kernel.defmodule(module) do
+          @moduledoc false
+          @doc false
+          @spec using?() :: true
+          def using?() do
+            true
+          end
+        end
+      error ->
+        raise "this sould not happen: #{inspect(error)}"
+    end
+
+    quote do
+    end
+  end
+
+  @doc """
+  Takes a `%Macro.Env{}` struct, and returns the same struct, if the struct's module has declared `use MaximumJank`.  If the module is not using `MaximumJank`, a RuntimeError will be raised.
+
+  ##Notes
+
+  - For some examples, see the tests.
+  - Refer to the docs for `&__using__/1` to understand why you'd use this function.
+  """
+  def __using__!(%Macro.Env{module: nil}) do
+    inspected_func = inspected_func(__ENV__)
+    raise "cannot invoke #{inspected_func} outside module"
+  end
+
+  def __using__!(%Macro.Env{}=env) do
+    if __using__?(env) do
+      env
+    else
+      module = inspect(__ENV__.module)
+      inspected_func = inspected_func(__ENV__)
+      raise "you must declare 'use #{module}' to invoke #{inspected_func}"
+    end
+  end
+
+  @doc """
+  Takes a `%Macro.Env{}` struct, and returns `true` the struct's module has declared `use MaximumJank`.  If the module is not using `MaximumJank`, it returns false.
+
+  ## Notes
+
+  - For some examples, see the tests.
+  - Refer to the docs for `&__using__/1` to understand why you'd use this function.
+  """
+  def __using__?(%Macro.Env{module: nil}) do
+    inspected_func = inspected_func(__ENV__)
+    raise "cannot invoke #{inspected_func} outside module"
+  end
+
+  def __using__?(%Macro.Env{}=env) do
+    module = Module.concat([env.module, MaximumJank])
+    module.using?()
+  rescue
+    _ -> false
+  end
+
+  @doc """
   Returns `arg` if it's a module, and raises a `RuntimeError  ` for anything else.
   """
   @spec module!(any()) :: module()
@@ -26,4 +106,16 @@ defmodule MaximumJank do
   end
 
   def module?(_), do: false
+
+  ###########
+  # Private #
+  ###########
+
+  defp inspected_func(%Macro.Env{}=env) do
+    module = inspect(env.module)
+    {function,arity} = env.function
+    function2 = Atom.to_string(function)
+    arity2 = Integer.to_string(arity)
+    "&" <> module <> "." <> function2 <> "/" <> arity2
+  end
 end
--- test/maximum_jank_test.exs
+++ test/maximum_jank_test.exs
@@ -1,6 +1,120 @@
 defmodule MaximumJankTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
   import MaximumJank
+
+  describe "&__using__/1" do
+    test "raises an error is used outside module"do
+      pattern = ~r/cannot invoke 'use MaximumJank' outside module/
+      func = fn -> Code.compile_string("use MaximumJank") end
+      assert_raise RuntimeError, pattern, func
+    end
+
+    test "using once inside module does not raise error" do
+      file =
+        """
+        defmodule UsingOnce do
+          use MaximumJank
+        end
+        """
+      assert Code.compile_string(file)
+    end
+
+    test "using twice inside module does not raise error" do
+      file =
+        """
+        defmodule UsingTwice do
+          use MaximumJank
+          use MaximumJank
+        end
+        """
+      assert Code.compile_string(file)
+    end
+  end
+
+  describe "&__using__!/1" do
+    test "raises error if not in module" do
+      pattern = ~r/cannot invoke &MaximumJank.__using__!\/1 outside module/
+      func = fn -> Code.compile_string("MaximumJank.__using__!(__ENV__)") end
+      assert_raise RuntimeError, pattern, func
+    end
+
+    test "raises error if without 'use MaximumJank'" do
+      file =
+        """
+        defmodule InNotUsing do
+          MaximumJank.__using__!(__ENV__)
+        end
+        """
+      pattern = ~r/you must declare 'use MaximumJank' to invoke &MaximumJank.__using__!\/1/
+      func = fn -> Code.compile_string(file) end
+      assert_raise RuntimeError, pattern, func
+    end
+
+    test "raises error if 'require' is used" do
+      file =
+        """
+        defmodule Requiring do
+          require MaximumJank
+          MaximumJank.__using__!(__ENV__)
+        end
+        """
+      pattern = ~r/you must declare 'use MaximumJank' to invoke &MaximumJank.__using__!\/1/
+      func = fn -> Code.compile_string(file) end
+      assert_raise RuntimeError, pattern, func
+    end
+
+    test "raises error if 'import' is used" do
+      file =
+        """
+        defmodule Importing do
+          import MaximumJank
+          MaximumJank.__using__!(__ENV__)
+        end
+        """
+      pattern = ~r/you must declare 'use MaximumJank' to invoke &MaximumJank.__using__!\/1/
+      func = fn -> Code.compile_string(file) end
+      assert_raise RuntimeError, pattern, func
+    end
+  end
+
+  describe "&__using__?/1" do
+
+    test "returns false if not using" do
+      func = fn ->
+        file =
+          """
+          defmodule NotUsing do
+            require Logger
+            _ = MaximumJank.__using__?(__ENV__) |> Logger.error()
+          end
+          """
+        Code.compile_string(file)
+      end
+      assert capture_log(func) =~ "false"
+    end
+    @tag :potato
+    test "returns true if using" do
+      func = fn ->
+        file =
+          """
+          defmodule Using do
+            require Logger
+            use MaximumJank
+            _ = MaximumJank.__using__?(__ENV__) |> Logger.error()
+          end
+          """
+        Code.compile_string(file)
+      end
+      assert capture_log(func) =~ "true"
+    end
+
+    test "raises error if not in module" do
+      pattern = ~r/cannot invoke &MaximumJank.__using__\?\/1 outside module/
+      func = fn -> Code.compile_string("MaximumJank.__using__?(__ENV__)") end
+      assert_raise RuntimeError, pattern, func
+    end
+  end
 
   describe "&module!/1" do
     test "returns module" do
